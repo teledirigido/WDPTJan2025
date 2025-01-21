f9fbec6e59285bfa84cdad0597e45711ae01ef77
unknown <eric.coimbra@gmail.com>
2025-01-21 19:31:57 +0000

    strings and variables

--- W2D1/string.js
+++ W2D1/string.js
@@ -1,43 +1,44 @@
-let user = 'Eric';
+let user = 'Eric'; //Double-quotes and single-quotes have rougly the same funcionality
 let dog = 'Meli';
 let dogBreed = 'German Shepherd';
 
-let oldPhrase = user + ' has a dog named ' + dog;
-/* console.log(oldPhrase); */
+let concatenated = user + ' has a dog named ' + dog; //String concatenation
+console.log(concatenated);
 
-let newPhrase = `${user} has a dog named ${dog}`;
-/* console.log(newPhrase); */
+let interpolated = `${user} has a dog named ${dog}`;
+console.log(interpolated);
 
 //String methods:
 
 //[index] notation:
-/* console.log(user[0] + user[1] + user[2] + user[3]); */
+console.log(user[0] + user[1] + user[2] + user[3]); //[index] notation returns the character at the position index. index starts at 0
 
 //.length
 const randomStr =
   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur, impedit, nemo similique nesciunt eos nobis autem molestiae amet repudiandae, debitis provident magnam iusto possimus praesentium rerum officia culpa vel5';
 
+//.lenght returns a number, the number of characters in the string
 const numOfLetter = randomStr.length;
 /* console.log(randomStr[numOfLetter - 1]); */
 
-//.slice()
-let sliced = dogBreed.slice(0, 6);
-/* console.log(sliced); */
+//.slice() cuts a section of the string, and returns the section without modifying the original string. syntax: string.slice(startIndex, endIndex)
+let sliced = dogBreed.slice(0, 6); //starting at index 0, stopping at index 6
+console.log(sliced);
 
-let sliced2 = dogBreed.slice(7);
-/* console.log(sliced2); */
+let sliced2 = dogBreed.slice(7); //starting at index 7 and going until the end of the string
+console.log(sliced2);
 
-let sliced3 = dogBreed.slice(-6);
-/* console.log(sliced3); */
+let sliced3 = dogBreed.slice(-6); //The last 6 characters of the string
+console.log(sliced3);
 
-//.toUpperCase()
+//.toUpperCase() takes the sring and return the full string in upper case
 let upperCasedName = user.toUpperCase();
 /* console.log(upperCasedName); */
 
-//.toLowerCase()
+//.toLowerCase() takes the sring and return the full string in lower case
 let lowerCasedName = user.toLowerCase();
 /* console.log(lowerCasedName); */
 
-//.includes()
+//string.includes(str) returns true or false depending if the str exists inside string.
 let doesInclude = 'Hello'.includes('h');
 console.log(doesInclude);
---
+++ W2D1/variables.js
@@ -0,0 +1,20 @@
+let x = 10; //Declare and initialize a variable
+let y; //Declare a variable but not initilize
+y = 20; //let can be reassinged
+
+let a = 5,
+  b = 10,
+  c = 15;
+//declaring multiple variables as once
+
+console.log(a, b, c);
+
+const pi = 3.14; //const can't be declared and not initialized
+/* pi = 5; */ //Throws an error, as const can't be reassinged
+
+const user = 'Eric',
+  age = 28,
+  isAdmin = true;
+//Declaring multiple constants at once
+
+console.log(pi, user, age, isAdmin);
