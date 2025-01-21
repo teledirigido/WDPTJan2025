3b4c7ab43060a4be5d08d44d24728d1ec0f08fd5
unknown <eric.coimbra@gmail.com>
2025-01-21 19:47:09 +0000

    logic operands and numbers

--- W2D1/logical.js
+++ W2D1/logical.js
@@ -1,23 +1,29 @@
-//two equals
-/* console.log(0 == '0'); */
+//two equals evaluates if the value is the same, even if the type isn't.
+console.log(0 == '0'); //true
+console.log(0 == 1); //false
 
-//three equals
-/* console.log(0 === '0');
-console.log(0 === 0); */
+//three equals evaluates if the value and type are the same.
+console.log(0 === '0'); //false
+console.log(0 === 0); //true
 
-//gt, lt, let, get
-/* console.log(1 < 1);
-console.log(1 <= 0);
-console.log(1 > 1);
-console.log(1 >= 1); */
+//greater, lesser, greater equal and lesser equal
+console.log(0 < 1); //true
+console.log(0 > 1); //false
+console.log(0 >= 1); //false
+console.log(1 <= 1); //true
 
-//OR ||
-/* console.log(1 > 2 || 0 > 1 || 5 > 3); */
+//OR || evaluates and responds with true if at least one value is truthy
+console.log(true || false); //true
+console.log(true || true); //true
+console.log(false || false); //false
 
-//AND &&
-/* console.log(3 > 2 && 2 > 1 && 5 > 3); */
+//AND && evaluates and responds with true if all values are truthy
+console.log(true && false); //false
+console.log(false && false); //false
+console.log(true && true); //true
 
-//Not !
+//Not ! will reverse the truthy or falsy value
 console.log(!false);
+console.log(!true);
 
-console.log(!!'');
+console.log(!!''); //very useful if you forget the truthy value of some value
--- W2D1/numbers.js
+++ W2D1/numbers.js
@@ -1,36 +1,38 @@
 let number = 0.4;
 
-//Math.round()
-/* console.log(Math.round(number)); */
+//Math.round() will round the number on mathematical logic (greater than or equal to 0.5 goes up, rest goes down)
+console.log(Math.round(number));
 
-//Math.ceil()
-/* console.log(Math.ceil(number)); */
+//Math.ceil() will always round up
+console.log(Math.ceil(number));
 
-//Math.floor()
-/* console.log(Math.floor(number)); */
+//Math.floor() will always round down
+console.log(Math.floor(number));
 
-//Math.min()
-/* console.log(Math.min(0, 4, 5, -2, 100, -50)); */
+//Math.min() takes a list of numbers and returns the smallest
+console.log(Math.min(0, 4, 5, -2, 100, -50));
 
-//Math.max()
-/* console.log(Math.max(0, 4, 100, 5, -2, 100, -50, 100)); */
+//Math.max() takes a list of numbers and returns the biggest
+console.log(Math.max(0, 4, 100, 5, -2, 100, -50, 100));
 
-//Math.random()
-//console.log(Math.random() /*  * 10 + 5 */);
+//Math.random() returns a random number between 0 and 1, not including 1. Can be modified to change the treshold with mathematical operations and Math.floor
+console.log(Math.random() /*  * 10 + 5 */);
 
+//Mathematical Operations
 //addition
-/* console.log(1 + 1); */
+console.log(1 + 1);
 
 //subtraction
-/* console.log(1 - 1); */
+console.log(1 - 1);
 
 //division
-/* console.log(4 / 2); */
+console.log(4 / 2);
 
 //multiplication
-/* console.log(5 * 2); */
+console.log(5 * 2);
 
-/* number = 1;
+//can also be done with += -= /^= *= to reassign values
+number = 1;
 
 number += 4;
 console.log(number);
@@ -42,11 +44,11 @@ number /= 2;
 console.log(number);
 
 number *= 2;
-console.log(number); */
+console.log(number);
 
-//Raising to a power
-/* console.log(3 ** 3); */
+//Raising to a power **
+console.log(3 ** 3);
 
-//Module operator.
+//Module operator. Divides and returns the remainder. Very useful to check for odd or even
 console.log(3 % 2);
 console.log(2 % 2);
