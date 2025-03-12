c21680846b26d1dfb0db9e8f86f2931f12c844be
Miguel Garrido <hi@miguel.nz>
2025-03-12 11:53:58 +0100

    better explanation on destructuring and spread operator

--- W6D1/destructuring-rest/example.js
+++ W6D1/destructuring-rest/example.js
@@ -3,18 +3,44 @@ const person = {
   age: 45
 }
 
-// new way to create and assign variables
+/**
+ * New way to create and assign variables
+ * 
+**/
 let { firstName: myVar, age: personAge } = person;
-console.log(myVar);
-// // console.log(firstName);
 
-// const person2 = person;
-// person2.firstName = "Bob Dylan"; // we updated person 2 firstname to Bob Dylan
+console.log(firstName); // John Doe
 
-// console.log(person2.firstName); // Bob DYlan
-// console.log(person); 
+const person2 = person;
+person2.firstName = "Bob Dylan";
 
-// let person3 = {...person}; // copy person into person3
+console.log(person2.firstName); // Bob Dylan
+console.log(person.firstName);  // Bob Dylan
 
-// person3.firstName = "Elvis Persley";
-// console.log(person3);
+/**
+ * 
+ * ⬆️
+ * 
+ * Why person.firstName is BobDylan, if on line 2 we see is "John Doe"?
+ * 
+ * This happens because on how we assigned person on line 14. 
+ * person2 and person will be 'pointing' to the same block of memory, 
+ * this means that any change on a property will affect person and person2 
+ * 
+ */
+
+let person3 = {...person}; // Here we create a copy of person
+
+person3.firstName = "Elvis Presley";
+console.log(person3);
+
+/**
+ * ⬆️
+ * 
+ * Here person3 firstName is Elvis Presley while person keeps 
+ * its original name. This is because of the spread operator on line 32.
+ * 
+ * This is a safe way to assign objects into a new variable.
+ * 
+ */
+
