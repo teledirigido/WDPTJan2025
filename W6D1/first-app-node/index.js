09d73f152d3e5890e7d5e423ff45341ef2c370c9
Miguel Garrido <hi@miguel.nz>
2025-03-12 11:47:14 +0100

    first-node-app comments updated

--- W6D1/first-app-node/index.js
+++ W6D1/first-app-node/index.js
@@ -1,10 +1,15 @@
 /** 
  * 
  * 
+ * If you want to create/handle folders and file via command line:
  * 
- * mkdir [your-folder-name]: creates a folder | eg: `new-folder mkdir`
- * touch [your-file-name-with-extension]: create a file eg: `touch my-file.js`
+ * $ mkdir [your-folder-name]               # creates a folder | eg: `new-folder mkdir`
+ * $ touch [your-file-name-with-extension]  # create a file eg: `touch my-file.js`
  * 
+ * Useful commands
+ * 
+ * $ pwd      # to see the full path of your folder
+ * $ open .   # to open the current folder on your Finder / Explorer
  * 
  * */ 
 
